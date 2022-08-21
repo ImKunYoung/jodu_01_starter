5a7aaa7a567fe1a5d647019ec6dfe31a89c83245
ImKunYoung <lim8667@gmail.com>
2022-08-21 15:41:52 +0900

    HelloController 구현

---
+++ src/main/java/org/example/jodu_01_Starter/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.jodu_01_Starter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
