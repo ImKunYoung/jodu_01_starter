7b66e83a2549062060d5841bd23c076f419aa61f
ImKunYoung <lim8667@gmail.com>
2022-08-30 19:31:54 +0900

    이슈 수정 SessionUser 직렬화

--- src/main/java/org/example/jodu_01_Starter/config/auth/dto/SessionUser.java
+++ src/main/java/org/example/jodu_01_Starter/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package org.example.jodu_01_Starter.config.auth.dto;
 import lombok.Getter;
 import org.example.jodu_01_Starter.domain.user.User;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
